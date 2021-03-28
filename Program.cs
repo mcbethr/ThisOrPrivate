d029bd7626529411942f35d9860f5d43f8f537a6
mcbethr <51751681+mcbethr@users.noreply.github.com>
2021-03-28 06:46:26 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ThisOrPrivate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stopwatch stopwatch1 = new Stopwatch();
+            Stopwatch stopwatch2 = new Stopwatch();
+
+
+            stopwatch2.Start();
+            UsingThis UT = new UsingThis();
+            stopwatch2.Stop();
+
+            stopwatch1.Start();
+            DirectAssignment UU = new DirectAssignment(1);
+            stopwatch1.Stop();
+
+
+            Console.WriteLine("Direct Assignment: " + stopwatch1.ElapsedTicks);
+            Console.WriteLine("Using This:" + stopwatch2.ElapsedTicks);
+
+        }
+    }
+
+    public class DirectAssignment
+    {
+
+        int _example;
+
+        public DirectAssignment(int incomingExample)
+        {
+            _example = incomingExample;
+        }
+
+    }
+
+    public class UsingThis
+    {
+
+        int example;
+
+        public void UsingThisAssignment(int incomingExample)
+        {
+            this.example = incomingExample;
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ThisOrAssignment
+ IsThisOrAssignment
